677dc53d1365dd95ca6487311618d38000d33f3b
Naimish Mishra <naimish2121@gmail.com>
2023-08-09 00:11:58 +0530

    1

---
+++ offline.js
@@ -0,0 +1,27 @@
+﻿{
+	"version": 1691516599,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/preview_idle-sheet0.png",
+		"images/preview_idle-sheet1.png",
+		"images/sprite-sheet0.png",
+		"images/pngtreeredbulletlaunchillustrationimage_-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"media/mixkit-game-gun-shot-1662.ogg",
+		"media/zombie-growl-3-6863.ogg",
+		"media/new_project.ogg",
+		"media/untitled.ogg",
+		"media/23.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"pathfind.js"
+	]
+}
